04e073b72ab137b10ba74c503c5b1b8e961d376e
Nevets <12600557+Nevets10@users.noreply.github.com>
2023-03-11 19:01:49 -0800

    Refactor current functions, add newPlayer function
    It makes sense to me to have everything contained in one parent module
    that controls the flow of the game and then only revealing certain
    functions as user input is needed.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const gameBoard = (() => {
+const game = (() => {
 	const _board = {
 		a1: null,
 		a2: null,
@@ -11,25 +11,38 @@ const gameBoard = (() => {
 		c3: null,
 	};
 
-	let _isPlayer1 = true;
+	let _isPlayer1Turn = true;
+	let _player1;
+	let _player2;
+
+	const _playerFactory = (newName, isPlayerHuman = true) => {
+		const name = newName;
+		const isHuman = isPlayerHuman;
+		let score = 0;
+		return { name, isHuman, score };
+	};
+
+	const newPlayer = (playerName) => {
+		if (_player1 === undefined) {
+			_player1 = _playerFactory(playerName);
+			return `${playerName} is player 1.`;
+		} else if (_player2 === undefined) {
+			_player2 = _playerFactory(playerName);
+			return `${playerName} is player 2.`;
+		} else {
+			return "Players already set.";
+		}
+	};
 
 	const updateBoard = (playerChoice) => {
 		if (_board[playerChoice] !== null) {
 			return "Invalid choice.";
 		} else {
-			_board[playerChoice] = _isPlayer1 ? "X" : "O";
-			_isPlayer1 = !_isPlayer1;
+			_board[playerChoice] = _isPlayer1Turn ? "X" : "O";
+			_isPlayer1Turn = !_isPlayer1Turn;
 			return _board;
 		}
 	};
 
-	return { updateBoard };
+	return { newPlayer, updateBoard };
 })();
-
-const playerFactory = (newName, isPlayerHuman) => {
-	const name = newName;
-	const isHuman = isPlayerHuman;
-	let score = 0;
-
-	return { name, isHuman, score };
-};
