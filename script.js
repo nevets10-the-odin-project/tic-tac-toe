4db156606705d2392784f97b54adb93e5458b0b8
Nevets <12600557+Nevets10@users.noreply.github.com>
2023-03-11 17:23:52 -0800

    Create gameBoard module
    The project prompt says to use an array for the game board inside of
    the module, but it seems like it would be easier to check for the win
    condition if an object is used instead. There's only 9 spaces and 8
    possible win conditions, so I'm not dealing with a lot of data here.

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("Let the games begin.");
+const gameBoard = (() => {
+	const board = {
+		a1: null,
+		a2: null,
+		a3: null,
+		b1: null,
+		b2: null,
+		b3: null,
+		c1: null,
+		c2: null,
+		c3: null,
+	};
+
+	let isPlayer1 = true;
+
+	const updateBoard = (playerChoice) => {
+		if (board[playerChoice] !== null) {
+			return "Invalid choice.";
+		} else {
+			board[playerChoice] = isPlayer1 ? "X" : "O";
+			isPlayer1 = !isPlayer1;
+			return board;
+		}
+	};
+
+	return { updateBoard };
+})();
